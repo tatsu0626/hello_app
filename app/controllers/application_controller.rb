b19c3a810e54f83edbab0f846b36511860ceab0f
矢部立也 <yabetatsuya@mba.local>
2017-12-16 23:30:15 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+def hello
+	render html:"Hello,world!"
+end
+
+def goodbye
+	render html:"goodbye,world"
+end
+
+
+end
